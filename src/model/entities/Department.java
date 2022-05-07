86c7ad05a433a9c752f927e5e0ca160a5b1d30e0
rafaelspa <rsdepaula67@gmail.com>
2022-05-07 13:23:25 -0300

    Entities completed

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,5 +1,47 @@
 package model.entities;
 
-public class Department {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Department {
+	private String name;
+	private int payDay;
+	private List<Employee> employees = new ArrayList<>(); 
+	
+	public Department(String name, int payDay) {
+		this.name = name;
+		this.payDay = payDay;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPayDay() {
+		return payDay;
+	}
+
+	public void setPayDay(int payDay) {
+		this.payDay = payDay;
+	}
+	
+	public void addEmployee(Employee employee) {
+		employees.add(employee);
+	}
+	
+	public void removeEmployee(Employee employee) {
+		employees.remove(employee);
+	}
+	
+	public double payroll() {
+		double sum = 0.0;
+		for (Employee emp : employees) {
+			sum += emp.getSalary();
+		}
+		return sum;
+	}
 }
